a7cf2b7da3622082985f2ea3de01df9c7711a6ab
Dzming Li <i@dzming.li>
2025-08-31 01:38:19 +0800

    eoa

--- default.nix
+++ default.nix
@@ -17,5 +17,5 @@
   moonbit = pkgs.callPackage ./pkgs/moonbit { };
   huiwen-mincho = pkgs.callPackage ./pkgs/huiwen-mincho { };
   genryumin = pkgs.callPackage ./pkgs/genryumin { };
-  wemeet-bin-bwrap-wayland-screenshare = pkgs.libsForQt5.callPackage ./pkgs/wemeet-bin-bwrap { useWaylandScreenshare = true; };
+  TWRUDMincho = pkgs.callPackage ./pkgs/TWRUDMincho { };
 }
---
+++ pkgs/TRWUDMincho/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, stdenv, ... }:
+
+stdenv.mkDerivation {
+  pname = "TWRUDMincho";
+  version = "1.001";
+  src = ./.;
+
+  installPhase = ''
+    mkdir -p $out/share/fonts/opentype/
+    cp -r *.otf $out/share/fonts/opentype/
+
+  '';
+
+  meta = with pkgs.lib; {
+    description = "TRW UD 明朝体";
+    homepage = "https://zhuanlan.zhihu.com/p/9920115795";
+    license = licenses.ofl; 
+    platforms = platforms.all;
+  };
+}
